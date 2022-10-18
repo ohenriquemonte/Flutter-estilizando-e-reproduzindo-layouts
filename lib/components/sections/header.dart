54aa9316851bfd0b4790a62685e2fbed2d9ed2ae
Henrique Montenegro <henrique.montenegro@slcagricola.com.br>
2022-10-17 21:19:36 -0300

    02 - Estilizando Widgets

--- lib/components/sections/header.dart
+++ lib/components/sections/header.dart
@@ -6,16 +6,45 @@ class Header extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Row(
-        children: [
-          Column(
-            children: const [
-              Text('\$'),
-              Text('Balanço disponível'),
-            ],
-          ),
-          const Icon(Icons.account_circle),
-        ],
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color.fromRGBO(103, 99, 234, 1),
+            Color.fromRGBO(155, 105, 254, 1),
+            Color.fromRGBO(195, 107, 255, 1),
+          ],
+        ),
+        borderRadius: BorderRadius.vertical(bottom: Radius.circular(15)),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(16, 80, 16, 16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                // Text('\$1000.00', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                Text.rich(
+                  TextSpan(
+                    text: '\$',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                    ),
+                    children: [
+                      TextSpan(text: ' 1000.00', style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold, color: Colors.white)),
+                    ],
+                  ),
+                ),
+                Text('Balanço disponível', style: TextStyle(fontSize: 16)),
+              ],
+            ),
+            const Icon(Icons.account_circle, size: 48),
+          ],
+        ),
       ),
     );
   }
